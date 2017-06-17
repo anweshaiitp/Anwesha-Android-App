84d91b1906ddb7f276eb5efe6491a12cafd8a756
Mayank Vaidya <mayank8318@gmail.com>
2017-06-17 15:13:54 +0530

    Add tabs and scrollable image slide to home view

---
+++ app/src/main/java/com/iitp/mayank/celesta2k17/adapters/EventsAdapter.java
@@ -0,0 +1,48 @@
+package com.iitp.mayank.celesta2k17.adapters;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.iitp.mayank.celesta2k17.R;
+
+/**
+ * Created by mayank on 17/6/17.
+ */
+
+public class EventsAdapter extends PagerAdapter {
+    Context context;
+     int events[] = {R.drawable.ic_home_black_24dp,
+                    R.drawable.ic_poll_black_24dp};
+
+    public EventsAdapter(Context context){
+        this.context=context;
+    }
+
+    @Override
+    public int getCount() {
+        return events.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((ImageView) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(context);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageResource(events[position]);
+        ((ViewPager) container).addView(imageView,0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((ImageView) object);
+    }
+}
--- app/src/main/java/com/iitp/mayank/celesta2k17/fragments/HomePage.java
+++ app/src/main/java/com/iitp/mayank/celesta2k17/fragments/HomePage.java
@@ -3,11 +3,13 @@ package com.iitp.mayank.celesta2k17.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.iitp.mayank.celesta2k17.R;
+import com.iitp.mayank.celesta2k17.adapters.EventsAdapter;
 
 /**
  * Created by mayank on 26/5/17.
@@ -18,6 +20,10 @@ public class HomePage extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home,container,false);
+        ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.events_pager);
+        EventsAdapter eventsAdapter = new EventsAdapter(getContext());
+
+        viewPager.setAdapter(eventsAdapter);
         return rootView;
     }
 }
