300e1bdbd72cef5b3ed050f19ca4da1f666f75ff
Mayank Vaidya <mayank8318@gmail.com>
2017-08-11 17:20:26 +0530

    Add FAB for map

--- app/src/main/java/com/iitp/mayank/celesta2k17/fragments/HomePage.java
+++ app/src/main/java/com/iitp/mayank/celesta2k17/fragments/HomePage.java
@@ -1,8 +1,10 @@
 package com.iitp.mayank.celesta2k17.fragments;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,11 +22,20 @@ public class HomePage extends android.support.v4.app.Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.events_pager);
         EventsAdapter eventsAdapter = new EventsAdapter(getContext());
 
         viewPager.setAdapter(eventsAdapter);
+        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab_maps);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uri = "https://www.google.com/maps/@?api=1&map_action=map&center=25.535752,84.851065&zoom=16&basemap=satellite";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                getContext().startActivity(intent);
+            }
+        });
         return rootView;
     }
 }
