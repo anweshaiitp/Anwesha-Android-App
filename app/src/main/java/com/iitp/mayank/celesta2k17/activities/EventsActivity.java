156a21919ed157f882e9e31cfb57e45d0dd57a46
Mukuntha N S <muks14x@gmail.com>
2017-08-12 20:50:08 +0530

    Beautify Events Page

--- app/src/main/java/com/iitp/mayank/celesta2k17/activities/EventsActivity.java
+++ app/src/main/java/com/iitp/mayank/celesta2k17/activities/EventsActivity.java
@@ -1,11 +1,15 @@
 package com.iitp.mayank.celesta2k17.activities;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Window;
 
 import com.iitp.mayank.celesta2k17.R;
 import com.iitp.mayank.celesta2k17.adapters.EventsRecyclerViewAdapter;
@@ -17,6 +21,7 @@ public class EventsActivity extends AppCompatActivity implements EventsRecyclerV
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_events);
 
         recyclerView = (RecyclerView)findViewById(R.id.rv_events);
@@ -25,6 +30,10 @@ public class EventsActivity extends AppCompatActivity implements EventsRecyclerV
 
         recyclerView.setHasFixedSize(true);
         Resources resources = getResources();
+
+        android.support.v7.app.ActionBar bar = getSupportActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.colorEvents)));
+
         eventsRecyclerViewAdapter = new EventsRecyclerViewAdapter(this , resources.getStringArray(R.array.array_event_headers ),
                                                                   resources.getStringArray(R.array.array_event_text) ,
                                                                   resources.getStringArray(R.array.array_event_intent));
