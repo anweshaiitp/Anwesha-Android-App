6f48f6f2b680205ab7277d0df2df6d9b55b52fba
Mayank Vaidya <mayank8318@gmail.com>
2017-08-15 13:26:55 +0530

    Add events of each club

---
+++ app/src/main/java/com/iitp/mayank/celesta2k17/activities/ThresholdEvents.java
@@ -0,0 +1,46 @@
+package com.iitp.mayank.celesta2k17.activities;
+
+import android.content.res.Resources;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Window;
+
+import com.iitp.mayank.celesta2k17.R;
+import com.iitp.mayank.celesta2k17.adapters.EventsRecyclerViewAdapter;
+
+public class ThresholdEvents extends AppCompatActivity implements EventsRecyclerViewAdapter.ListCardClick {
+
+    RecyclerView recyclerView;
+    EventsRecyclerViewAdapter eventsRecyclerViewAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        setContentView(R.layout.activity_njack);
+
+        recyclerView = (RecyclerView)findViewById(R.id.rv_events);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        recyclerView.setHasFixedSize(true);
+        Resources resources = getResources();
+
+        android.support.v7.app.ActionBar bar = getSupportActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.colorEvents)));
+
+        eventsRecyclerViewAdapter = new EventsRecyclerViewAdapter(getApplicationContext() ,this , resources.getStringArray(R.array.array_threshold_event_headers),
+                resources.getStringArray(R.array.array_threshold_event_text) ,
+                resources.getStringArray(R.array.array_threshold_event_intent),
+                resources.obtainTypedArray(R.array.array_threshold_event_images));
+        recyclerView.setAdapter(eventsRecyclerViewAdapter);
+    }
+
+    @Override
+    public void onListClick(String intent) throws ClassNotFoundException {
+
+    }
+}
