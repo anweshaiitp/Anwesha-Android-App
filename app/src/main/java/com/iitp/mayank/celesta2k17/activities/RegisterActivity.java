cb152b3048ec3e7e9afb22778895f04d3087456b
Mayank Vaidya <mayank8318@gmail.com>
2017-08-17 19:44:12 +0530

    Create Acitivities for sign in and register

---
+++ app/src/main/java/com/iitp/mayank/celesta2k17/activities/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.iitp.mayank.celesta2k17.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iitp.mayank.celesta2k17.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
---
+++ app/src/main/java/com/iitp/mayank/celesta2k17/activities/SignInActivity.java
@@ -0,0 +1,15 @@
+package com.iitp.mayank.celesta2k17.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iitp.mayank.celesta2k17.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
