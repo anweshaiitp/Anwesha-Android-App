6109537e0941d90cd77dfc7e8bb3e9f06c5f9609
ivary43 <contactmanish16@gmail.com>
2017-08-30 23:58:41 +0530

    added navigation button in app developers page

--- app/src/main/java/com/iitp/mayank/celesta2k17/adapters/HighlightsRecylerViewAdapter.java
+++ app/src/main/java/com/iitp/mayank/celesta2k17/adapters/HighlightsRecylerViewAdapter.java
@@ -1,8 +1,49 @@
 package com.iitp.mayank.celesta2k17.adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.iitp.mayank.celesta2k17.R;
+
 /**
  * Created by manish on 30/8/17.
  */
 
-public class HighlightsRecylerViewAdapter {
+public class HighlightsRecylerViewAdapter extends RecyclerView.Adapter<HighlightsRecylerViewAdapter.HighlightsViewHolder> {
+
+
+    @Override
+    public HighlightsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public void onBindViewHolder(HighlightsViewHolder holder, int position) {
+
+
+    }
+
+    //making a view holder to cache the value of the view
+    class HighlightsViewHolder extends RecyclerView.ViewHolder {
+        TextView header;
+        TextView content;
+
+
+        public HighlightsViewHolder(View itemView) {
+            super(itemView);
+            header = (TextView) itemView.findViewById(R.id.highlightsheader);
+            content = (TextView) itemView.findViewById(R.id.highlightscontent);
+
+
+        }
+
+    }
+
 }
