50c2cb9a0ad3e101e8d41ec054367b39e978f970
ivary43 <contactmanish16@gmail.com>
2017-09-03 17:15:06 +0530

    added photoview dependency for click on zoom

--- app/src/main/java/com/iitp/mayank/celesta2k17/adapters/HighlightsRecylerViewAdapter.java
+++ app/src/main/java/com/iitp/mayank/celesta2k17/adapters/HighlightsRecylerViewAdapter.java
@@ -2,6 +2,7 @@ package com.iitp.mayank.celesta2k17.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Slide;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +26,7 @@ public class HighlightsRecylerViewAdapter extends RecyclerView.Adapter<Highlight
     {
         this.highlights=highlights ;
     }
+
 
 
 
