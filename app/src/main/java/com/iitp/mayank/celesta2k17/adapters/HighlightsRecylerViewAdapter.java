4a38c3a894f4b1320f1afe2c043e80a6e73c1667
Mayank Vaidya <mayank8318@gmail.com>
2017-09-23 17:13:50 +0530

    Check for empty highlights

--- app/src/main/java/com/iitp/mayank/celesta2k17/adapters/HighlightsRecylerViewAdapter.java
+++ app/src/main/java/com/iitp/mayank/celesta2k17/adapters/HighlightsRecylerViewAdapter.java
@@ -21,34 +21,61 @@ public class HighlightsRecylerViewAdapter extends RecyclerView.Adapter<Highlight
     Context context;
     ArrayList<String> header;
     ArrayList<String> details;
+
     //to set the values tossed from network splash background task
-    public HighlightsRecylerViewAdapter (Context context , ArrayList<String> header , ArrayList<String> details)
-    {
+    public HighlightsRecylerViewAdapter(Context context, ArrayList<String> header, ArrayList<String> details) {
         this.context = context;
-        this.header = header;
-        this.details = details;
+        if (header == null) {
+            this.header = null;
+            this.details = null;
+        } else {
+            this.header = new ArrayList<>(header);
+            this.details = new ArrayList<>(details);
+        }
     }
 
+    public void swap(ArrayList<String> h, ArrayList<String> d) {
+        if (header != null) {
+            header.clear();
+            details.clear();
 
+            if (h == null) {
+                header = null;
+                details = null;
+            } else {
+                header.addAll(h);
+                details.addAll(d);
+            }
+        } else {
+            if (h == null) {
+                this.header = null;
+                this.details = null;
+            } else {
+                this.header = new ArrayList<>(h);
+                this.details = new ArrayList<>(d);
+            }
+        }
+        notifyDataSetChanged();
+    }
 
 
     @Override
     public HighlightsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        Context context = parent.getContext() ;
+        Context context = parent.getContext();
 
-        LayoutInflater layoutInflater = LayoutInflater.from(context) ;
-        View view = layoutInflater.inflate(R.layout.highlights_card_view,parent,false);
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.highlights_card_view, parent, false);
         //tossing the view for caching
-        HighlightsViewHolder highlightsViewHolder = new HighlightsViewHolder(view) ;
+        HighlightsViewHolder highlightsViewHolder = new HighlightsViewHolder(view);
 
-        return  highlightsViewHolder ;
+        return highlightsViewHolder;
 
     }
 
     @Override
     public int getItemCount() {
-        return header.size();
+        return header == null ? 0 : header.size();
     }
 
     @Override
--- app/src/main/java/com/iitp/mayank/celesta2k17/fragments/HighlightsPage.java
+++ app/src/main/java/com/iitp/mayank/celesta2k17/fragments/HighlightsPage.java
@@ -11,6 +11,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.iitp.mayank.celesta2k17.R;
@@ -31,7 +32,8 @@ import java.util.StringTokenizer;
 
 public class HighlightsPage extends android.support.v4.app.Fragment {
 
-    RecyclerView highlightsRecylerView ;
+    TextView emptyTextView;
+    RecyclerView highlightsRecylerView;
     HighlightsRecylerViewAdapter highlightsRecylerViewAdapter;
     ArrayList<String> header = new ArrayList<>();
     ArrayList<String> details = new ArrayList<>();
@@ -45,72 +47,82 @@ public class HighlightsPage extends android.support.v4.app.Fragment {
         View rootView = inflater.inflate(R.layout.fragment_highlights, container, false);
         directory = new ContextWrapper(getContext()).getDir("highlights", Context.MODE_PRIVATE);
 
-        setData();
-        swipeRefreshLayout = (SwipeRefreshLayout)rootView.findViewById(R.id.swiperefresh_highlights);
+        highlightsRecylerView = (RecyclerView) rootView.findViewById(R.id.highlightsRecylerView);
+        emptyTextView = (TextView) rootView.findViewById(R.id.empty_highlight_text_view);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swiperefresh_highlights);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                new UpdateHighlightsAsyncTask().execute(new ContextWrapper(getContext()) , getContext());
+                new UpdateHighlightsAsyncTask().execute(new ContextWrapper(getContext()), getContext());
                 swipeRefreshLayout.setRefreshing(false);
             }
         });
 
-        highlightsRecylerView=(RecyclerView)rootView.findViewById(R.id.highlightsRecylerView) ;
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         highlightsRecylerView.setLayoutManager(linearLayoutManager);
 
-        highlightsRecylerViewAdapter = new HighlightsRecylerViewAdapter(getContext() , header , details);
+        highlightsRecylerViewAdapter = new HighlightsRecylerViewAdapter(getContext(), header, details);
         highlightsRecylerView.setAdapter(highlightsRecylerViewAdapter);
 
+        checkEmpty();
+//        setData();
         return rootView;
     }
 
+    private void checkEmpty() {
+        if (header == null || header.size() == 0) {
+            emptyTextView.setVisibility(View.VISIBLE);
+            highlightsRecylerView.setVisibility(View.GONE);
+        } else {
+            emptyTextView.setVisibility(View.GONE);
+            highlightsRecylerView.setVisibility(View.VISIBLE);
+        }
+    }
+
     private void setData() {
-        File file = new File(directory , "highlight.txt");
+        File file = new File(directory, "highlight.txt");
         try {
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String x;
-            while((x = bufferedReader.readLine()) != null)
-            {
+            while ((x = bufferedReader.readLine()) != null) {
                 System.out.println(x);
-                StringTokenizer stringTokenizer = new StringTokenizer(x , ":-");
+                StringTokenizer stringTokenizer = new StringTokenizer(x, ":-");
                 header.add(stringTokenizer.nextToken());
                 details.add(stringTokenizer.nextToken());
             }
             bufferedReader.close();
             fileReader.close();
+            checkEmpty();
+            highlightsRecylerViewAdapter.swap(header, details);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void clearData()
-    {
+    private void clearData() {
         header.clear();
         details.clear();
+        highlightsRecylerViewAdapter.swap(header, details);
     }
 
-    private class UpdateHighlightsAsyncTask extends AsyncTask<Object,Void,Boolean>{
+    private class UpdateHighlightsAsyncTask extends AsyncTask<Object, Void, Boolean> {
         @Override
         protected void onPostExecute(Boolean aBoolean) {
             super.onPostExecute(aBoolean);
-            if(aBoolean)
-            {
+            if (aBoolean) {
                 clearData();
-                highlightsRecylerViewAdapter.notifyDataSetChanged();
                 setData();
-                highlightsRecylerViewAdapter.notifyDataSetChanged();
                 Toast.makeText(getContext(), "Highlights updated successfully", Toast.LENGTH_SHORT).show();
-            }
-            else
+            } else
                 Toast.makeText(getContext(), "No network connection. Please try again later.", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         protected Boolean doInBackground(Object... params) {
-            return  new NetworkUtils().extractHighlights((ContextWrapper) params[0] , (Context)params[1]) ;
+            return new NetworkUtils().extractHighlights((ContextWrapper) params[0], (Context) params[1]);
         }
     }
 }
