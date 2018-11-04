0dde538c6f602b274baa1209568cfad5e9b20f6a
avisheksin <avisheksin1999@gmail.com>
2018-11-05 01:08:13 +0530

    changed status bar color and added event_card_new.xml file

--- app/src/main/java/info/anwesha2k18/iitp/activities/MainActivity.java
+++ app/src/main/java/info/anwesha2k18/iitp/activities/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
         Window window = getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(ContextCompat.getColor(MainActivity.this,R.color.cardview_dark_background));
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         navigationView.setNavigationItemSelectedListener(item -> {
 
