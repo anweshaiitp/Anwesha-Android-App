a5ab45ef487fae0e3e2be79f6f8ef8eb033e8c80
Anirban <nandi.anirban11@gmail.com>
2018-11-12 21:33:07 +0530

    added new login screen

--- app/src/main/java/info/anwesha2k18/iitp/activities/LoginActivity.java
+++ app/src/main/java/info/anwesha2k18/iitp/activities/LoginActivity.java
@@ -1,79 +1,94 @@
 package info.anwesha2k18.iitp.activities;
 
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
-import android.content.pm.ActivityInfo;
-import android.graphics.Bitmap;
-import android.graphics.Point;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.annotation.ColorRes;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Display;
 import android.view.View;
-import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.ImageViewTarget;
+import info.anwesha2k18.iitp.fragments.LogInFragment;
+import info.anwesha2k18.iitp.fragments.SignUpFragment;
 
-import java.util.List;
-
-import butterknife.BindViews;
-import butterknife.ButterKnife;
 import info.anwesha2k18.iitp.R;
-import info.anwesha2k18.iitp.adapters.AuthAdapter;
-import info.anwesha2k18.iitp.listeners.AnimatedViewPagerAuth;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+import static info.anwesha2k18.iitp.FlexibleFrameLayout.ORDER_LOGIN_STATE;
+import static info.anwesha2k18.iitp.FlexibleFrameLayout.ORDER_SIGN_UP_STATE;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @BindViews(value = {R.id.logo,R.id.first,R.id.second,R.id.last})
-    protected List<ImageView> sharedElements;
+    private info.anwesha2k18.iitp.databinding.ActivityLoginBinding binding;
+    private boolean isLogin = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        ButterKnife.bind(this);
-        final AnimatedViewPagerAuth pager= ButterKnife.findById(this,R.id.pager);
-        final ImageView background= ButterKnife.findById(this,R.id.scrolling_background);
-        int[] screenSize=screenSize();
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
 
-        for(ImageView element:sharedElements){
-            @ColorRes int color=element.getId()!=R.id.logo?R.color.white_transparent:R.color.color_logo_log_in;
-            DrawableCompat.setTint(element.getDrawable(), ContextCompat.getColor(this,color));
+        LogInFragment topLoginFragment = new LogInFragment();
+        SignUpFragment topSignUpFragment = new SignUpFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.login_fragment, topLoginFragment)
+                .replace(R.id.sign_up_fragment, topSignUpFragment)
+                .commit();
+
+        binding.loginFragment.setRotation(-90);
+
+        binding.button.setOnSignUpListener(topSignUpFragment);
+        binding.button.setOnLoginListener(topLoginFragment);
+
+        binding.button.setOnButtonSwitched(isLogin -> {
+            binding.getRoot()
+                    .setBackgroundColor(ContextCompat.getColor(
+                            this,
+                            isLogin ? R.color.signUpColor : R.color.loginColor));
+        });
+
+        binding.loginFragment.setVisibility(INVISIBLE);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        binding.loginFragment.setPivotX(binding.loginFragment.getWidth() / 2);
+        binding.loginFragment.setPivotY(binding.loginFragment.getHeight());
+        binding.signUpFragment.setPivotX(binding.signUpFragment.getWidth() / 2);
+        binding.signUpFragment.setPivotY(binding.signUpFragment.getHeight());
+    }
+
+    public void switchFragment(View v) {
+        if (isLogin) {
+            binding.loginFragment.setVisibility(VISIBLE);
+            getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.loginColor));
+            binding.loginFragment.animate().rotation(0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    binding.signUpFragment.setVisibility(INVISIBLE);
+                    binding.signUpFragment.setRotation(90);
+                    binding.wrapper.setDrawOrder(ORDER_LOGIN_STATE);
+                }
+            });
+        } else {
+            binding.signUpFragment.setVisibility(VISIBLE);
+            getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.signUpColor));
+            binding.signUpFragment.animate().rotation(0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    binding.loginFragment.setVisibility(INVISIBLE);
+                    binding.loginFragment.setRotation(-90);
+                    binding.wrapper.setDrawOrder(ORDER_SIGN_UP_STATE);
+                }
+            });
         }
-        //load a very big image and resize it, so it fits our needs
-        Glide.with(this)
-                .asBitmap()
-                .load(R.drawable.busy)
-                .into(new ImageViewTarget<Bitmap>(background) {
-                    @Override
-                    protected void setResource(Bitmap resource) {
-                        background.setImageBitmap(resource);
-                        background.post(()->{
-                            //we need to scroll to the very left edge of the image
-                            //fire the scale animation
-                            background.scrollTo(-background.getWidth()/2,0);
-                            ObjectAnimator xAnimator= ObjectAnimator.ofFloat(background, View.SCALE_X,4f,background.getScaleX());
-                            ObjectAnimator yAnimator= ObjectAnimator.ofFloat(background, View.SCALE_Y,4f,background.getScaleY());
-                            AnimatorSet set=new AnimatorSet();
-                            set.playTogether(xAnimator,yAnimator);
-                            set.setDuration(getResources().getInteger(R.integer.duration));
-                            set.start();
-                        });
-                        pager.post(()->{
-                            AuthAdapter adapter = new AuthAdapter(getSupportFragmentManager(), pager, background, sharedElements);
-                            pager.setAdapter(adapter);
-                        });
-                    }
-                });
+
+        isLogin = !isLogin;
+        binding.button.startAnimation();
     }
 
-    private int[] screenSize(){
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        return new int[]{size.x,size.y};
-    }
 }
