c482ba547e99610557600aa3ebc19489272d7140
VatsalSin <vatsal.eliot@gmail.com>
2019-01-22 00:59:21 +0530

    changed developers fragment

--- app/src/main/java/info/anwesha2k18/iitp/fragments/DevelopersFragment.java
+++ app/src/main/java/info/anwesha2k18/iitp/fragments/DevelopersFragment.java
@@ -30,28 +30,23 @@ public class DevelopersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_developers, container, false);
-        ImageView github_muks = (ImageView) rootView.findViewById(R.id.github_muks);
-        ImageView github_mayank = (ImageView) rootView.findViewById(R.id.github_mayank);
+
+        ImageView github_vatsal = (ImageView) rootView.findViewById(R.id.github_vatsal);
         ImageView github_manish = (ImageView) rootView.findViewById(R.id.github_manish);
-        ImageView fb_muks = (ImageView) rootView.findViewById(R.id.fb_muks);
-        ImageView fb_mayank = (ImageView) rootView.findViewById(R.id.fb_mayank);
+        ImageView fb_vatsal = (ImageView) rootView.findViewById(R.id.fb_vatsal);
         ImageView fb_manish = (ImageView) rootView.findViewById(R.id.fb_manish);
 
         String urls[] = {
-                "https://github.com/Muks14x",
-                "https://github.com/Mayank8318",
                 "https://github.com/ivary43",
-                "https://www.facebook.com/pesk123",
-                "https://www.facebook.com/mayank.vaidya.9",
-                "https://www.facebook.com/profile.php?id=100009684360848"
+                "https://github.com/vatsalsin",
+                "https://www.facebook.com/profile.php?id=100009684360848",
+                "https://www.facebook.com/vatsalsinghal1"
         };
 
-        github_muks.setOnClickListener(new devOnClickListener(urls[0]));
-        github_mayank.setOnClickListener(new devOnClickListener(urls[1]));
-        github_manish.setOnClickListener(new devOnClickListener(urls[2]));
-        fb_muks.setOnClickListener(new devOnClickListener(urls[3]));
-        fb_mayank.setOnClickListener(new devOnClickListener(urls[4]));
-        fb_manish.setOnClickListener(new devOnClickListener(urls[5]));
+        github_vatsal.setOnClickListener(new devOnClickListener(urls[1]));
+        github_manish.setOnClickListener(new devOnClickListener(urls[0]));
+        fb_vatsal.setOnClickListener(new devOnClickListener(urls[3]));
+        fb_manish.setOnClickListener(new devOnClickListener(urls[2]));
 
         return rootView;
     }
